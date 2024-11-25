3cfc5eb201a20c8577adcde9edf3e16e8eef6b55
fmarcos38 <fmarcos_23@hotmail.com>
2024-11-25 20:38:01 -0300

    fav

--- src/componentes/ListaFavoritos/index.jsx
+++ src/componentes/ListaFavoritos/index.jsx
@@ -8,43 +8,40 @@ function ListaFavoritos({allProps}) {
 
     return (
         <div className='cont-listaProps-fav'>
-            <h1 className='titulo-lista-props-fav'>Propiedades Favoritas</h1>
-            
-                    <div className='cont-card-lista-props'>                        
-                        {
-                            allProps[0] ?
-                            allProps.map(p => {
-                                return (
-                                    <div className='cont-card-Fav-listaProps' key={p._id}>
-                                        <Card
-                                            key={p._id}
-                                            id={p._id}
-                                            tituloPublicacion={p.tituloPublicacion}
-                                            ubicacion={p.ubicacion}
-                                            operacion={p.operacion}
-                                            imagenes={p.imagenes}
-                                            cantCocheras={p.cantCocheras}
-                                            ambientes={p.ambientes}
-                                            dormitorios={p.dormitorios}
-                                            tipoPropiedad={p.tipoPropiedad}
-                                            supTotal={p.supTotal}
-                                            supDescubierta={p.supDescubierta}
-                                            supCubierta={p.supCubierta}
-                                            supSemiCub={p.supSemiCub}
-                                            baños={p.baños}
-                                        />
-                                    </div>
-                                )
-                            }) : (
-                                <div className='no-props'>
-                                    <NoHayProps/>
+            <h1 className='titulo-lista-props-fav'>Tus Propiedades Favoritas</h1>
+
+            <div className='cont-card-lista-props'>
+                {
+                    allProps[0] ?
+                        allProps.map(p => {
+                            return (
+                                <div className='cont-card-Fav-listaProps' key={p._id}>
+                                    <Card
+                                        key={p._id}
+                                        id={p._id}
+                                        tituloPublicacion={p.tituloPublicacion}
+                                        ubicacion={p.ubicacion}
+                                        operacion={p.operacion}
+                                        imagenes={p.imagenes}
+                                        cantCocheras={p.cantCocheras}
+                                        ambientes={p.ambientes}
+                                        dormitorios={p.dormitorios}
+                                        tipoPropiedad={p.tipoPropiedad}
+                                        supTotal={p.supTotal}
+                                        supDescubierta={p.supDescubierta}
+                                        supCubierta={p.supCubierta}
+                                        supSemiCub={p.supSemiCub}
+                                        baños={p.baños}
+                                    />
                                 </div>
                             )
-                        }
-                    </div>
-                
-            
-            
+                        }) : (
+                            <div className='no-props'>
+                                <NoHayProps />
+                            </div>
+                        )
+                }
+            </div>
         </div>
     )
 }
